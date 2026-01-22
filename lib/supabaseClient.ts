7784bcd1b8bda8c6342510fdab22378efdbe990a
HafizWrG <132629477+HafizWrG@users.noreply.github.com>
2026-01-22 16:59:47 +0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Tambahkan pengecekan ini agar error lebih jelas di local development
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase URL or Anon Key")
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
